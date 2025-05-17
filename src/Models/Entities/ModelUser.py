dac813703ff4fd4bcf04240287360acc7a4faf8e
David Rocha <150465868+wickuzi@users.noreply.github.com>
2025-05-17 12:38:38 -0600

    Update ModelUser.py

--- src/Models/Entities/ModelUser.py
+++ src/Models/Entities/ModelUser.py
@@ -4,30 +4,32 @@ from werkzeug.security import generate_password_hash, check_password_hash
 class ModelUser:
 
     @classmethod
-    def register(cls, db, user):
+    def register(cls, conn, user):
         try:
-            cursor = db.connection.cursor()
+            cursor = conn.cursor()
             sql_check = "SELECT id FROM user WHERE correo = %s"
             cursor.execute(sql_check, (user.correo,))
             if cursor.fetchone():
+                cursor.close()
                 return "Correo ya registrado"
 
             # Cifrar la contraseña antes de guardarla
             hashed_password = generate_password_hash(user.password)
             sql_insert = "INSERT INTO user (username, correo, password) VALUES (%s, %s, %s)"
             cursor.execute(sql_insert, (user.username, user.correo, hashed_password))
-            db.connection.commit()
+            conn.commit()
             return "OK"
         except Exception as ex:
             return f"Error: {ex}"
 
     @classmethod
-    def login(cls, db, user):
+    def login(cls, conn, user):
         try:
-            cursor = db.connection.cursor()
+            cursor = conn.cursor()
             sql = "SELECT id, username, correo, password FROM user WHERE correo = %s"
             cursor.execute(sql, (user.correo,))
             row = cursor.fetchone()
+            cursor.close()
 
             if row is not None:
                 hash_from_db = row[3]
@@ -39,12 +41,13 @@ class ModelUser:
             raise Exception(f"Error al hacer login: {ex}")
         
     @classmethod
-    def get_by_id(self, db, id):
+    def get_by_id(cls, conn, id):
         try:
-            cursor = db.connection.cursor()
-            sql = "SELECT id, username, correo FROM user WHERE id = {}".format(id)
-            cursor.execute(sql)
+            cursor = conn.cursor()
+            sql = "SELECT id, username, correo FROM user WHERE id = %s"
+            cursor.execute(sql,(id,))
             row = cursor.fetchone()
+            cursor.close()
 
             if row is not None:
                     return User(row[0], row[1], row[2], None)
