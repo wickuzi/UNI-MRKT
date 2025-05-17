ee5e4c6f44cbef7aa16023dd57b9c0b95012bf2a
David Rocha <150465868+wickuzi@users.noreply.github.com>
2025-05-17 12:23:41 -0600

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash,session
 from config import config
-from flask_mysqldb import MySQL
+import mysql.connector
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from Models.Entities.ModelUser import ModelUser
